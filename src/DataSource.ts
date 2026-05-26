0e4fd90d26597bf76a58b4e57594001e37c4e9d1
João Matos <joaomatos@criticalmanufacturing.com>
2026-05-26 17:29:52 +0100

    URI encode variables and manually handle :singlequote

--- src/DataSource.ts
+++ src/DataSource.ts
@@ -8,14 +8,48 @@ export class ODataSource extends DataSourceWithBackend<ODataQuery, ODataOptions>
     super(instanceSettings);
   }
 
+  /**
+   * 
+   * @param query The ODATA query
+   * @param scopedVars Dashboard/panel variables like including time ranges or intervals
+   * @returns the query with variables replaced by their values
+   */
   applyTemplateVariables(query: ODataQuery, scopedVars: ScopedVars) {
     const templateSrv = getTemplateSrv();
+    const singleQuoteFlags: boolean[] = []; // track which variable occurrences had :singlequote, in order
+    const applySingleQuote = (value: string) => `'${value}'`; // No need to do .replace(/'/g, "''") because encodeODataValue is always called first
+    const encodeODataValue = (value: string) => encodeURIComponent(value).replace(/'/g, "''"); // apply percent encoding and escape single quotes by duplicating them
+
+    // try to match ${varName:format}
+    const stripped = query.oDataQueryString?.replace(/\$\{([^:}]+)(?::([^}]+))?\}/g, (match, varName, format) => {
+      if (format === 'singlequote') {
+        singleQuoteFlags.push(true);
+        return `\${${varName}}`; // strip the :singlequote
+      }
+      singleQuoteFlags.push(false);
+      return match; // leave other formats intact
+    });
 
     query.filterConditions?.forEach((filterCondition) => {
       filterCondition.value = templateSrv.replace(filterCondition.value, scopedVars);
     });
 
-    const oDataQueryString = templateSrv.replace(query.oDataQueryString, scopedVars);
+    let callIndex = 0;
+    const oDataQueryString = templateSrv.replace(stripped, scopedVars, (value: string | string[]) => {
+      const hadSingleQuote = singleQuoteFlags[callIndex++]; // used to check if the string must be surrounded by single quotes
+
+      if (Array.isArray(value)) {
+        return value
+          .map(v => {
+            const encoded = encodeODataValue(v);
+            return hadSingleQuote ? applySingleQuote(encoded) : encoded;
+          })
+          .join(',');
+      }
+
+      const encoded = encodeODataValue(value);
+      return hadSingleQuote ? applySingleQuote(encoded) : encoded;
+    });
 
     return {
       ...query,
